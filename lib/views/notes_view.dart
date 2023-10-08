ac60adecde520a7337d9ace06234d2d119f50bff
Swagnik02 <swagnik1234@gmail.com>
2023-10-08 17:20:24 +0530

    clean

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,11 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:notes/constants/routes.dart';
 import 'package:notes/enums/menu_action.dart';
-// import 'dart:developer' as devtools show log;
-
-// import 'package:notes/views/login_view.dart';
 
 class NotesView extends StatefulWidget {
   const NotesView({super.key});
