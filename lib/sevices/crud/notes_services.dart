ff6fb0a40f6a421d43d716a03591e11475cde59c
Swagnik02 <swagnik1234@gmail.com>
2023-10-11 19:01:03 +0530

    added queries

--- lib/sevices/crud/notes_services.dart
+++ lib/sevices/crud/notes_services.dart
@@ -23,22 +23,17 @@ class NoteService {
       final dbPath = join(docsPath.path, dbName);
       final db = await openDatabase(dbPath);
       _db = db;
+
+      // CREATE USER TABLE
+      await db.execute(createUserTable);
+
+      // CREATE NOTE TABLE
+      await db.execute(createNoteTable);
     } on MissingPlatformDirectoryException {
       throw UnableToGetDoccumentsDirectory();
     }
   }
 }
-
-// constants
-const noteTable = 'note';
-const userTable = 'user';
-
-const idColumn = 'id';
-const emailColumn = 'email';
-
-const userIdColumn = 'user_id';
-const textColumn = 'text';
-const isSyncedWithCloudColumn = 'is_synced_with_cloud';
 
 @immutable
 class DatabaseUser {
@@ -93,3 +88,33 @@ class DatabaseNote {
   @override
   int get hashCode => id.hashCode;
 }
+
+// constants
+const userTable = 'user';
+const noteTable = 'note';
+
+const idColumn = 'id';
+const emailColumn = 'email';
+
+const userIdColumn = 'user_id';
+const textColumn = 'text';
+const isSyncedWithCloudColumn = 'is_synced_with_cloud';
+
+// SQL QUERIES
+const createUserTable = '''
+                CREATE TABLE IF NOT EXISTS "user" (
+                	"id"	INTEGER NOT NULL,
+                	"email"	TEXT NOT NULL UNIQUE,
+                	PRIMARY KEY("id" AUTOINCREMENT)
+                );
+      ''';
+const createNoteTable = '''
+              CREATE TABLE IF NOT EXISTS "note" (
+              	"id"	INTEGER NOT NULL,
+              	"user_id"	INTEGER NOT NULL,
+              	"text"	TEXT,
+              	"is_synced_with_cloud"	INTEGER NOT NULL DEFAULT 0,
+              	FOREIGN KEY("user_id") REFERENCES "user"("id"),
+              	PRIMARY KEY("id" AUTOINCREMENT)
+              );
+      ''';
